5400f3b3ef903f2fb115b8fa00580b29b186243d
Ajmal-k <ajmalekvsroad@gmail.com>
2019-05-29 22:15:22 +0530

    added tfrecords

---
+++ utils/generate_tfrecord.py
@@ -0,0 +1,103 @@
+# Create train data:
+# python generate_tfrecord.py --label=<LABEL> --csv_input=<PATH_TO_ANNOTATIONS_FOLDER>/train_labels.csv --img_path=<PATH_TO_IMAGES_FOLDER>/train  --output_path=<PATH_TO_ANNOTATIONS_FOLDER>/train.record
+# example:
+# python generate_tfrecord.py --label0=pistol --label1=rifle --label2=shotgun --csv_input=../annotations/train_labels.csv --img_path=images/train  --output_path=../annotations/train.record
+
+#  Create test data:
+# python generate_tfrecord.py --label=<LABEL> --csv_input=<PATH_TO_ANNOTATIONS_FOLDER>/test_labels.csv --img_path=<PATH_TO_IMAGES_FOLDER>/test --output_path=<PATH_TO_ANNOTATIONS_FOLDER>/test.record
+# example:
+# python generate_tfrecord.py --label0=pistol --label1=rifle --label2=shotgun --csv_input=../annotations/test_labels.csv --img_path=images/test  --output_path=../annotations/test.record
+
+import os
+import io
+import pandas as pd
+import tensorflow as tf
+
+from PIL import Image
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+
+import sys
+# sys.path.append("../../models/research")
+
+flags = tf.app.flags
+flags.DEFINE_string('csv_input', '', 'path to the csv input')
+flags.DEFINE_string('output_path', '', 'path to the tfrecord output')
+flags.DEFINE_string('label0', '', 'name of class[0] label')
+flags.DEFINE_string('label1', '', 'name of class[1] label')
+flags.DEFINE_string('label2', '', 'name of class[2] label')
+
+flags.DEFINE_string('img_path', '', 'path to images')
+FLAGS = flags.FLAGS
+
+def class_text_to_int(row_label):
+    if row_label == FLAGS.label0:
+        return 0
+    elif row_label == FLAGS.label1:
+        return 1
+    elif row_label == FLAGS.label2:
+        return 2
+    else:
+        None
+
+def split(df, group):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)),'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+def main():
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    path = os.getcwd()
+    parent_path = os.path.abspath(os.path.join(path, os.pardir))
+    path = os.path.join(parent_path, FLAGS.img_path)
+    examples = pd.read_csv(FLAGS.csv_input)
+    grouped = split(examples, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, path)
+        writer.write(tf_example.SerializeToString())
+
+    writer.close()
+    output_path = os.path.join(parent_path, FLAGS.output_path)
+    print(f'Successfully converted the TFRcords: {output_path}')
+
+main()
